bd881d4af8675e7746c7fb63df1f5774f9788c9c
Drishya <90393584+drishyaa5@users.noreply.github.com>
2024-03-30 10:32:13 +0545

    Update server.js

--- server.js
+++ server.js
@@ -9,8 +9,14 @@ import categoryRoutes from './routes/categoryRoutes.js'
 import productRoutes from './routes/productRoutes.js'
 import cors from 'cors';
 import path from 'path';
+import {fileURLToPath} from 'url';
 //rest object
 const app = express();
+
+//esmodule fix
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
 
 //configure env
 dotenv.config();
