5c19c777144fb0dd96af2908cfeddae3d30545a6
Drishya <drishyaa005@gmail.com>
2024-03-30 10:01:00 +0545

    Update server.js

--- server.js
+++ server.js
@@ -22,7 +22,7 @@ connectDB();
 app.use(cors());
 app.use(express.json());
 app.use(morgan('dev'));
-app.use(express.static(path.join(__dirnamem,'./client/build')))
+app.use(express.static(path.join(__dirname,'./client/build')))
 
 //routes
 app.use('/api/v1/auth',authRoutes);
@@ -32,9 +32,9 @@ app.use('/api/v1/product',productRoutes);
 
 
 //rest api
-app.get('/',(req,res)=>{
-    res.send("<h1>This is bello world</h1>")
-});
+app.use('*',function(req,res){
+    res.sendFile(path.join(__dirname,'./client/build/index.html'));
+})
 
 const PORT = process.env.PORT || 8000;
 
