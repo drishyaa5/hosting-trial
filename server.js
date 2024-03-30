2f6f23f58741fd2b53c74570e0f4fbc25c7ec3e3
Drishya <drishyaa005@gmail.com>
2024-03-30 09:53:46 +0545

    build folder

--- server.js
+++ server.js
@@ -8,6 +8,7 @@ import authRoutes from './routes/authRoute.js'
 import categoryRoutes from './routes/categoryRoutes.js'
 import productRoutes from './routes/productRoutes.js'
 import cors from 'cors';
+import path from 'path';
 //rest object
 const app = express();
 
@@ -21,6 +22,7 @@ connectDB();
 app.use(cors());
 app.use(express.json());
 app.use(morgan('dev'));
+app.use(express.static(path.join(__dirnamem,'./client/build')))
 
 //routes
 app.use('/api/v1/auth',authRoutes);
